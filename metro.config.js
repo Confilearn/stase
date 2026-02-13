d4dbd9721032f2f7e5e68c805f811c1f8a98612d
Confilearn <confilearn@gmail.com>
2026-02-13 10:12:31 +0100

    Change splash icon and debug react-native-svg error

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
+
+const config = getDefaultConfig(__dirname);
+
+// Add SVG transformer
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+config.resolver.assetExts.push('svg');
+
 module.exports = withNativeWind(config, { input: './app/global.css' })
